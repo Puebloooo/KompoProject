483a31d897bd35079992eceeb4fe9d4a110f29bb
Pueblo <pawel.kosmala94@gmail.com>
2016-12-27 20:10:21 +0100

    Speeding Up And Better Ground Detection

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,26 +4,43 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
     private float jumpTimeCounter;
+    private float speedMilestoneCount;
     private Animator playerAnimator;
-    private Collider2D playerCollider;
+    //private Collider2D playerCollider; 
     private Rigidbody2D playerRigidbody;
     
     public bool grounded;
     public float movementSpeed;
+    public float movementSpeedMultiplier;
+    public float speedIncreaseMilestone;
     public float jumpTime;
     public float jumpForce;
     public LayerMask layerMask;
-    
+    public Transform groundCheck;
+    public float groundCheckRadius;
 	// Use this for initialization
 	void Start () {
         playerRigidbody = GetComponent<Rigidbody2D>();
-        playerCollider = GetComponent<Collider2D>();
+        //playerCollider = GetComponent<Collider2D>(); // used for older version of ground detection
         playerAnimator = GetComponent<Animator>();
+
+        jumpTimeCounter = jumpTime;
+        speedMilestoneCount = speedIncreaseMilestone;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        grounded = Physics2D.IsTouchingLayers(playerCollider,layerMask);
+
+        //grounded = Physics2D.IsTouchingLayers(playerCollider,layerMask); 
+
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, layerMask); // revise name of layermask 
+
+        if (transform.position.x > speedMilestoneCount)
+        {
+            speedMilestoneCount += speedIncreaseMilestone;
+            speedIncreaseMilestone = speedIncreaseMilestone * movementSpeedMultiplier;
+            movementSpeed = movementSpeed * movementSpeedMultiplier;
+        }
 
         playerRigidbody.velocity = new Vector2(movementSpeed, playerRigidbody.velocity.y);
 
