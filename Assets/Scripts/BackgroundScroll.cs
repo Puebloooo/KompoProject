32d6233ec0fa173483234e2b4397eb7978e951ab
Pueblo <pawel.kosmala94@gmail.com>
2017-01-04 22:37:23 +0100

    Added Scrolling Background
    Incomplete...

--- Assets/Scripts/BackgroundScroll.cs
+++ Assets/Scripts/BackgroundScroll.cs
@@ -3,14 +3,20 @@ using System.Collections;
 
 public class BackgroundScroll : MonoBehaviour {
 
-    public
-	// Use this for initialization
-	void Start () {
-	
+    private Vector3 previousCameraPosition;
+    
+    public float smoothing;
+    public Transform background;
+    
+    void Start () {
+        previousCameraPosition = transform.position;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	void LateUpdate () {
+        Vector3 parallax = (previousCameraPosition - transform.position) * ((-10)/smoothing);
+        background.position = new Vector3(background.position.x + parallax.x, background.position.y, background.position.z);
+        previousCameraPosition = transform.position;
+               
 	}
+ 
 }
